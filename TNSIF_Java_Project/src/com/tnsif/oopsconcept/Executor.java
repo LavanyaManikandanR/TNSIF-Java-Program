526ec843a717a7b695882d99f39f555507e48c40
user <user@G-SYS13>
2025-07-17 12:11:59 +0530

    commit oops

---
+++ TNSIF_Java_Project/src/com/tnsif/oopsconcept/Executor.java
@@ -0,0 +1,17 @@
+package com.tnsif.oopsconcept;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		
+		Encapsulation ed = new Encapsulation();
+		ed.setName("MS Dhoni");
+		ed.setAge(43);
+		ed.setJersyNo(7);
+		System.out.println(ed);
+		
+		
+
+	}
+
+}
